e9396d63efbf83a190d3fd20b1bf11a7bc49ee58
ppoh71 <ppoh71@gmail.com>
2018-12-21 12:26:56 +0000

    Add a pin on a map when user taps and holds on a location
    This is an example app for a MapKit implementation.
    It adds an LongGesture to a MapView, the UIView lcoation is converted to a MapLocation
    and a Annotation is added to the Map

---
+++ AddPinOnTouchMap/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  AddPinOnTouchMap
+//
+//  Created by Peter Pohlmann on 21.12.18.
+//  Copyright © 2018 Peter Pohlmann. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        let longTapGesture = UILongPressGestureRecognizer(target: self, action: #selector(longTap))
+        mapView.addGestureRecognizer(longTapGesture)
+    }
+
+    @objc func longTap(sender: UIGestureRecognizer){
+        print("long tap")
+        if sender.state == .began {
+            let locationInView = sender.location(in: mapView)
+            let locationOnMap = mapView.convert(locationInView, toCoordinateFrom: mapView)
+            addAnnotation(location: locationOnMap)
+        }
+    }
+
+    func addAnnotation(location: CLLocationCoordinate2D){
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location
+            annotation.title = "Some Title"
+            annotation.subtitle = "Some Subtitle"
+            self.mapView.addAnnotation(annotation)
+    }
+}
+
+extension ViewController: MKMapViewDelegate{
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is MKPointAnnotation else { print("no mkpointannotaions"); return nil }
+        
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.rightCalloutAccessoryView = UIButton(type: .infoDark)
+            pinView!.pinTintColor = UIColor.black
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        return pinView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            if let doSomething = view.annotation?.title! {
+               print("do something")
+            }
+        }
+    }
+}
+
+
